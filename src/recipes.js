449173e23d5272a8e2a727f308fcef18a24baa1b
farah <farah.fournat@gmail.com>
2021-12-14 14:40:30 +0100

    useeffect fetch data

--- src/recipes.js
+++ src/recipes.js
@@ -1,11 +1,37 @@
 import React, { useState, useEffect } from 'react'
 
-const recipes = () => {
+const url ='https://www.themealdb.com/api/json/v1/1/search.php?f=a';
+
+
+const Recipes = () => {
+    const [recipes,setRecipes]= useState([]);
+    const getRecipes = async()=>{
+        const response = await fetch(url);
+        const recipes = await response.json();
+        setRecipes(recipes);
+    }
+
+    useEffect(()=>{
+        getRecipes();
+    },[])
     return (
-        <div>
-            <h1>Recipe App</h1>
-        </div>
+        <>
+            <h3>Recipes App</h3>
+            <ul className='users'>
+                {recipes.map((recipe)=>{
+                    const{idMeal,strMeal,strCategory,strMealThumb,strYoutube}=recipe
+                    return <li key={idMeal}>
+                        <img src={strMealThumb} alt={strMeal} />
+                        <div>
+                            <h4>{strMeal}</h4>
+                            <a href={strYoutube}>Watch Youtube recipe</a>
+                        </div>
+
+                    </li>
+                })}
+            </ul>
+        </>
     )
 }
 
-export default recipes
+export default Recipes
